38f439aced31e8c2e1c741ee7963effe909b64e7
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-07 21:23:15 +0900

    complete react tutorial

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Square = props => {
+  return(
+    <button
+      onClick={props.onClick}
+      className="square"
+    >
+      {props.value}
+    </button>
+  )
+}
+
+export default Square
